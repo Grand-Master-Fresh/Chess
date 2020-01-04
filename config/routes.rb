5a474d7bdc1f705e5507a29cb23c129a0e1f8017
Ulises Cadena <ucadena07@gmail.com>
2020-01-04 13:12:18 -0600

    Add Rubocop gem and config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+<<<<<<< HEAD
   devise_for :users
+=======
+Rails.application.routes.draw do
+>>>>>>> Add Rubocop gem and config files
   root 'static#landing'
-
 end
