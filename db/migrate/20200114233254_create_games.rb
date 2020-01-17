fecf600b6c5cc653f14010049b5a3ac65767aa5f
Julie Kovalaske <juliekovalaske@gmail.com>
2020-01-17 21:53:50 +0000

    update spelling in create games.rb and run rake db migrate

--- db/migrate/20200114233254_create_games.rb
+++ db/migrate/20200114233254_create_games.rb
@@ -1,11 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.integer :id
       t.integer :white_player_id
       t.integer :black_player_id
       t.string :name
-      t.ingeger :state
+      t.integer :state
       t.timestamps
     end
   end
--- db/migrate/20200114233522_create_pieces.rb
+++ db/migrate/20200114233522_create_pieces.rb
@@ -1,7 +1,6 @@
 class CreatePieces < ActiveRecord::Migration[5.2]
   def change
     create_table :pieces do |t|
-      t.integer :id
       t.string :type
       t.integer :x_position
       t.integer :y_position
