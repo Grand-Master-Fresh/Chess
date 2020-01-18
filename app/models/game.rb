22f7a4eae04ef03b07c288bfe07169822344d202
Julie Kovalaske <juliekovalaske@gmail.com>
2020-01-18 21:00:03 +0000

    updated models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,10 @@
 class Game < ApplicationRecord
-  scope :available, -> { where(black_player_id: nill) }
+  scope :available, -> { where(black_player_id: nil) }
 
-  belongs_to :white_player,
-    class_name: "User", 
-    foreign_key: "id"
+  belongs_to :white_player
+    
 
   belongs_to :black_player
-    class_name: "User",
-    foreign_key: "id"
+  
     
 end
