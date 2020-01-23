80972f529a5add7c797be25bafc8bd3ab7be3370
Ulises Cadena <ucadena07@gmail.com>
2020-01-22 18:43:33 -0600

    Fixed Associations

--- app/controllers/static_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class StaticController < ApplicationController
-  def show
-    render params[:page]
-  end
-end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "games#create" do
+
+   it "should require users to be logged in" do
+     game = FactoryBot.create(:game)
+     post :create
+     expect(response).to redirect_to new_user_session_path
+   end
+  end
+
+  it "should allow signed in users to create a new game in the database" do
+
+  end
+
+
+
 end
