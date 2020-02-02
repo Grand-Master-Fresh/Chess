2116f7ff70ce7e00dce6082e10637286bee4a9dc
Julie Kovalaske <juliekovalaske@gmail.com>
2020-02-02 18:42:51 +0000

    making rook move

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,10 @@
 class Rook < Piece
+    def valid_move?(x_destination, y_destination)
+    if (x_position == x_destination || y_position == y_destination) &&
+      !obstructed?([x_destination, y_destination])
+        @has_moved = true
+        true
+    else false
+    end
+  end
 end
